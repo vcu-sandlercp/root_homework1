a73129776dcf5a644ebc1a402442bcafdbbb7f8f
Collin Sandler <sandlercp@vcu.edu>
2021-02-15 11:54:05 -0500

    tested different ways to fix hw1 code

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -11,7 +11,7 @@ def return_number_3():
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
 
-    return_value = 'vcu'
+    return_value = "vcu"
     return return_value
 
 
@@ -20,19 +20,20 @@ def return_lowercased_string(input_string):
     Return it but the lowercase version of it."""
 
     return_value = input_string
-    return return_value
+    return return_value.lower
 
 
 def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
-    return_value = input_string
-    return return_value
+     input_string = "   testing string with whitespace   "
+    return input_string.strip
 
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
-
-    return_value = None
+    first_number = 1
+    second_number = 2
+    return_value = first_number + second_number
     return return_value
